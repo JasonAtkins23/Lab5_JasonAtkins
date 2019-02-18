3576b3fb31342eb7267ea05db1f3b7a6f340f164
Jason Atkins <jason.a.atkins-1@ou.edu>
2019-02-18 13:19:30 -0600

    Wrote the first part of the Todo.java class

--- Todo.java
+++ Todo.java
@@ -1,16 +1,22 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-02-18
+OU ID_______________: 113408630
+OU EMAIL____________: jason.a.atkins-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Write the calculateTwoTokens method in the Calculator Class
+2. Write the calculateThreeTokens method in the Calculator Class
+3. Write the execute method in the Calculator Class
+4. Write the parseAndExecute method in the Calculator Class
+5. Write all the exception methods in the CalculatorTest class
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 45
+2. 20
+3. 20
+4. 15
+5. 60
 
 Actual Time (minutes):
 1. XXX
